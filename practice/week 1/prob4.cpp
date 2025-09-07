05908010e78335e8ccd9419c9be1bb0e8e03d3e3
TirthNandha <ntirth15@gmail.com>
2025-09-07 21:41:43 +0530

    mte practice week 1 prob4 done

---
+++ practice/week 1/prob4.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int binarySearch(vector<int>& arr, int k){
+    int i = 0;
+    int j = arr.size() - 1;
+    int ans = -1;
+    while(i<=j){
+        int mid = i + (j-i)/2;
+        if(arr[mid]<=k){
+            ans = arr[mid];
+            i = mid+1;
+        }
+        else{
+            j = mid-1;
+        }
+    }
+    return ans;
+
+}
+
+int main(){
+    int n;
+    cout << "Enter n:" << endl;
+    cin >> n;
+    int k;
+    vector<int> arr(n);
+    cout << "Enter array elements: ";
+    for(int i= 0; i<n;i++){
+        cin >> arr[i];
+    }
+    cout << "Enter k: " << endl;
+    cin >> k;
+    cout << binarySearch(arr, k);
+
+    // for(int num: arr){
+    //     cout << num << endl;
+    // }
+
+}
